62093e0a6abe46ccfdf448d735a581f726f98d09
qwq140 <qwq140@gmail.com>
2021-01-15 15:37:52 +0900

    1. 스트링부터 RestController 개념잡기

---
+++ src/main/java/com/cos/person/domain/CommonDto.java
@@ -0,0 +1,23 @@
+package com.cos.person.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class CommonDto<T> {
+	private int statusCode;
+	private T data;
+	
+	public CommonDto(int statusCode, T data) {
+		super();
+		this.statusCode = statusCode;
+		this.data = data;
+	}
+	public CommonDto(int statusCode) {
+		super();
+		this.statusCode = statusCode;
+	}
+
+	
+}
